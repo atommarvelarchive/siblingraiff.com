c41654b1ac3aec8916013e60fbe4eb94c7c59cef
Atom Raiff <atom.raiff@gmail.com>
2015-08-18 07:37:29 -0700

    merged amy's website into this repo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var port = process.env.PORT || 5000;
-
 //setup express, mongo, and server vars
 var express = require("express"),
+    program = require('commander'),
     app = new express();    
 
 app.set('view engine', 'ejs');
@@ -11,10 +11,17 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(__dirname + '/views'));
 
+program
+  .version('0.0.1')
+  .option('-o, --aaron', 'aaron\'s personal website')
+  .option('-y, --amy', 'amy\'s personal website')
+  .parse(process.argv);
+
+var choice = program.aaron ? require("./aaron.json") : require("./amy.json");
 
 // Homepage
 app.get("/", function(req, res){	
-  res.render("home");	
+  res.render("home", {person:choice});	
 });
 
 
